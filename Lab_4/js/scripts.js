fa4bb798ad6294fd9b3deae896fc73153bbb0592
tvo1026 <tvo1026@github.com>
2020-10-07 23:18:38 -0400

    Update scripts.js

--- Lab_4/js/scripts.js
+++ Lab_4/js/scripts.js
@@ -1,4 +1,4 @@
-let name = "Thinh Vo"
+let name = "Thinh Vo";
 // 1 Change the content of the div with class "header" to "[Your name]'s Lab 4"
 const myName = document.querySelector(".header");
 myName.textContent = name + "'s Lab 4";
@@ -6,8 +6,10 @@ myName.textContent = name + "'s Lab 4";
 //  Label each paragraph with a distinct class name
 const paragraphs = document.querySelector(".content");
 let firstPa = document.createElement("p");
+firstPa.innerText = "";
 firstPa.className = "p1";
 let secondPa = document.createElement("p");
+secondPa.innerText = "";
 secondPa.className = "p2";
 paragraphs.appendChild(firstPa);
 paragraphs.appendChild(secondPa);
@@ -20,7 +22,7 @@ let firstLine = secondPa.append("the third character in my name is: " + name.cha
 secondPa.appendChild(document.createElement("br"));
 // 6 Add a new line to your second paragraph
 let secondLine = document.createElement("p");
-secondLine.innerText = ""
+secondLine.innerText = "";
 // 7 Return the final three characters of your last name to that new line
 secondLine = secondPa.append(name.slice(6,8));
 // 8 Substring your first and last name into two separate variables
