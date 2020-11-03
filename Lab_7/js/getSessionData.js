257b036538dbef7d26f88b0b1e247727ac52051b
tvo1026 <tvo1026@github.com>
2020-11-03 14:05:19 -0500

    lab 7 updated

--- Lab_7/js/getSessionData.js
+++ Lab_7/js/getSessionData.js
@@ -1,10 +1,9 @@
 // Get saved data from sessionStorage
-let name = ""; // Your code here
-let semester =  ""; // Your code here
-let credits =  ""; // Your code here
-let description =  ""; // Your code here 
+let title = ""; // Your code here
+let lat_max = "";
+let lat_min = "";
+let lon_max = "";
+let lon_min = "";
 
 // Your code here
 // Set the corresponding <p> elements in the GetSessionData.html page with the values from above
-
-
