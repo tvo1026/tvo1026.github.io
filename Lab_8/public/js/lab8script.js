6cbf1b38a22a6a7e2a0bfc8e2e97f19002262c47
tvo1026 <tvo1026@github.com>
2020-11-13 11:42:53 -0500

    lab8_completed

--- Lab_8/public/js/lab8script.js
+++ Lab_8/public/js/lab8script.js
@@ -1,4 +1,3 @@
-
 let url = "https://www.reddit.com/r/popular.json";
 let chart = "";
 console.log(url);
@@ -7,11 +6,19 @@ console.log(url);
 let settings = { method: "Get" };
 let chartValues = [];
 
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min) + min); //The maximum is exclusive and the minimum is inclusive
+  }
+
 async function getData() {
     await fetch(url, settings)
         .then(res => res.json())
         .then((json) => {
+            console.log(json);
             let listSize = json.data.children.length;
+            console.log(listSize);
             // Loop to pick 5 random entries
             for (x = 0; x < 5; x++) {
                 /*
@@ -22,16 +29,25 @@ async function getData() {
                     Add a new <li> element with the message to the 'redditList' element
                     Add a data entry to chartValues with author as the label and ups as the y component
                 */
-    
-
+               const random = getRandomInt(0, 25);
+               const post = json.data.children[random].data;
+               console.log(post);
+               let subreddit = post.subreddit;
+               let author = post.author;
+               let title = post.title;
+               let ups = post.ups;
+               let message = "<b>Subreddit </b>: " + subreddit + " <b>Author</b>:" + author + " <b>Title</b>:" + title + " <b>Up votes</b>: " + ups;
+               let ul = document.getElementById('redditList');
+               ul.innerHTML += "<li>" + message + "</li>";
                 /*.......*/
                 
                 let addToChart = {'label':author,y:ups}; // Gave this. This needs to be added to the 'chartValues'
+                chartValues.push(addToChart);
                 /*.......*/
             }
         })
         .then(values => console.log(chartValues));
-        //chart.render(); // Do you need to remove the comments from here in order to get it to work?
+        chart.render(); // Do you need to remove the comments from here in order to get it to work? Yes
 };
 
 window.onload = async function makeChart() {
@@ -45,7 +61,7 @@ window.onload = async function makeChart() {
             { 
                 type: "column",
                 name: "Popular Reddit",
-                dataPoints: // WHAT GOES HERE???
+                dataPoints: chartValues // WHAT GOES HERE???
             }
         ]
     });
--- Lab_8/server.js
+++ Lab_8/server.js
@@ -1,6 +1,15 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 
 // Code here for how to set 'public' as the static folder for express
-
+app.use(express.static('public'));
+/*
+app.get('/', function(request, response){
+    response.sendFile('public/index.html');
+});
+*/
 // Code here to have the app listen on port 8080....please provide a console.log message
+app.listen(8080, function(){
+    console.log("Started node app on 8080");
+});
+
