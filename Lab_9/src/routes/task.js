f6163d156e8eca910ed9fc7ede29cf64e602ea6d
tvo1026 <tvo1026@github.com>
2020-11-25 04:27:12 -0500

    lab 9 added

--- Lab_9/src/routes/task.js
+++ Lab_9/src/routes/task.js
@@ -33,12 +33,12 @@ router.post('/task', (req, res) => {
 
 //GET
 router.get('/task', (req, res) => {
-    if (!//TODO) {
+    if (!req.query.taskId) {
         return res.status(400).send('Missing URL parameter id')
     }
     let sql = "select * from tasklist where id = ?"
-    console.log("req.query.taskId: " + //TODO)
-    let params = [//TODO]
+    console.log("req.query.taskId: " + req.query.taskId)
+    let params = [req.query.taskId]
     db.get(sql, params, (err, row) => {
         if (err) {
           res.status(400).json({"error":err.message});
@@ -55,8 +55,8 @@ router.get('/task', (req, res) => {
 router.put('/task', (req, res) => {
     console.log("PUT called")
     var data = {
-        id : //TODO,
-        taskName: //TODO
+        id : req.query.taskId, //TODO
+        taskName: req.query.taskName //TODO
         
     }
     console.log("data.id:" + data.id + " name:" + data.taskName)
@@ -83,6 +83,23 @@ router.put('/task', (req, res) => {
 
 //Delete
 //TODO add entire DELETE method
-
-
+router.delete('/task', (req, res) => {
+    if (!req.query.taskId) {
+        return res.status(400).send('Missing URL parameter taskId')
+    }
+    db.run(
+        'DELETE FROM tasklist WHERE id = ?',
+        req.query.taskId,
+        function (err, result) {
+            if (err) {
+                res.status(400).json({"error": res.message})
+                return;
+            }
+            res.json({
+                "message":"success", 
+                changes: this.changes
+            })
+        }
+    )
+})
 module.exports = router
